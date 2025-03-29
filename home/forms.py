335716f2fb21fdcbd58917b34e22b7dbff42fd0a
Ialimeva <mevashy@gmail.com>
2025-03-29 16:59:27 +0300

    Add creation date and fix start date

--- home/forms.py
+++ home/forms.py
@@ -5,10 +5,11 @@ from .models import enquete, questions, responseSelection, enqueteResponse, repo
 class enqueteForm(forms.ModelForm):
     class Meta:
         model = enquete
-        fields = ['code', 'name', 'description', 'end_date']
+        fields = ['code', 'name', 'description', 'start_date', 'end_date']
         widgets = {
             'code' : forms.TextInput(attrs={'class' : ''}),
             'name' : forms.TextInput(attrs={'class' : ''}),
             'descriptiom' : forms.TextInput(attrs={'class' : ''}),
+            'start_date' : forms.DateInput(attrs={'class' : '', 'type' : 'date'}),
             'end_date' : forms.DateInput(attrs={'class' : '', 'type' : 'date'}),
         }
