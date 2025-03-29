87cccf148a4c66bfb5c021cf33edc300213d62f2
Ialimeva <mevashy@gmail.com>
2025-03-29 10:28:01 +0300

    Add all models

--- home/models.py
+++ home/models.py
@@ -1,13 +1,54 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+# Creation des roles
 class role(models.Model):
     role_name = models.CharField(max_length=15)
 
+# Connection entre role et user
 class role_and_user_connex(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
     roleID = models.ForeignKey(role, on_delete=models.CASCADE)
+    
+# Creation des enquetes
+class enquete(models.Model):
+    code = models.CharField(max_length = 10, unique = True)
+    name = models.CharField(max_length = 100, unique = True)
+    description = models.CharField(max_length = 255, null= True, blank= True)
+    status = models.CharField(max_length = 10)
+    start_date = models.DateField(auto_now_add = True)
+    end_date = models.DateField(null= False, blank= False)
+    userID = models.ForeignKey(User, on_delete = models.CASCADE, related_name= 'enquete')
+    token = models.UUIDField(default = uuid.uuid4, editable = False, unique= True)
+    
+# Creation des questions
+class questions(models.Model):
+    question = models.CharField(max_length = 255, unique = True )
+    description = models.CharField(max_length = 255, unique = True )
+    enqueteID = models.ForeignKey(enquete, on_delete= models.CASCADE, related_name= 'questions')
+    response_type = models.CharField(max_length = 50)
+    
+# Creation des reponses
+class responseSelection(models.Model):
+    reponse = models.CharField(max_length = 255)
+    note = models.IntegerField(max_length = 10)
+    questionID = models.ForeignKey(questions, on_delete= models.CASCADE, related_name= 'reponseSelection')
+
+# Creation des connections entre enquete, questions et reponses
+class enqueteResponse(models.Model):
+    email = models.EmailField(max_length= 50)
+    token = models.UUIDField(default= uuid.uuid4, editable= False, unique= True)
+    enqueteID = models.ForeignKey(enquete, on_delete= models.CASCADE, related_name= 'enqueteResponse')
+    status = models.CharField(max_length= 10)
+    responseDateTime = models.DateField(auto_now_add= True)
+    validationDateTime = models.DateField(null= False, blank= False)
+
+# Creation des reposes finals
+class reponses(models.Model):
+    enqueteResponseID = models.ForeignKey(enqueteResponse, on_delete= models.CASCADE, related_name= 'reponses')
+    questionID = models.ForeignKey(questions, on_delete= models.CASCADE, related_name= 'reponses')
+    responseSelectionID = models.ForeignKey(responseSelection, on_delete= models.CASCADE, related_name= 'reponses')
+    response_comment = models.CharField(max_length= 50)
+    
